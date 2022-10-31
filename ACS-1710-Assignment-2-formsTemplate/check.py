68a3d68943060d6f62f193788f60d05a238d3d99
Parul Bhardwaj <111934039+ParulK-bhardwaj@users.noreply.github.com>
2022-10-31 00:58:35 -0400

    Add files via upload

---
+++ ACS-1710-Assignment-2-formsTemplate/check.py
@@ -0,0 +1,6 @@
+def sort_letters(message):
+    """A helper method to sort the characters of a string in alphabetical order
+    and return the new string."""
+    return ''.join(sorted(list(message)))
+
+sort_letters('parul')
